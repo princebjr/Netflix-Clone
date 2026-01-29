6a1d85763297acfd326b88f0f78e325d6f3513a2
Prince Boateng <yb@yb.local>
2026-01-29 16:49:18 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './Pages/Home/Home'
+
+
+
+const App = () => {
+  return (
+    <div>
+
+      <Home/>
+
+      
+    </div>
+  )
+}
+
+export default App
