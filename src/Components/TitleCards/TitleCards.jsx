8d0c33bfa3346bb55028e9aa798c759395e9f10d
Prince Boateng <yb@yb.local>
2026-01-30 15:11:52 +0000

    New Changes

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef,useEffect } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+
+
+const TitleCards = (title,category) => {
+
+  const cardsRef = useRef();
+
+const handlewheel = (event) => {
+  event.preventDefault();
+  cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handlewheel);
+},[])
+
   return (
     <div className='title-cards'>
-        <h2>Popular on Netflix</h2>
-        <div className='card-list'>
+        <h2>{title?title:"Popular on Netflix"}</h2>
+        <div className='card-list' ref = {cardsRef}>
             {cards_data.map((card,index)=>{
                 return <div className='card' key={index}>
                     <img src={card.image} alt="" />
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
       <img src={hero_banner} alt="" className='banner-img' />
       <div className='hero-caption'>
         <img src={hero_title} alt="" className='caption-img'/>
-        <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the citu from immortal enemy.</p>
+        <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
         <div className='hero-btns'>
           <button className='btn'><img src={play_icon} alt="" />Play</button>
           <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
@@ -26,6 +26,13 @@ const Home = () => {
       </div>
     </div>
 
+    <div className="more-cards">
+      <TitleCards title = {'Blockbuster Movies'}/>
+      <TitleCards title = {'Only on Netflix'}/>
+      <TitleCards title = {'Uppcoming'}/>
+      <TitleCards title = {'To Picks for you'}/>
+    </div>
+    
     </div>
   )
 }
